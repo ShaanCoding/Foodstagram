d8f619e3ffa1038ce233d45930d990b4c5625c31
Jibreil <jibreilhoneine@gmail.com>
2022-08-16 15:43:50 +1000

    Integration test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello ASD World</h1>;
 }
 
 export default App;
