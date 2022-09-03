a200fb3956bb4089f781dac87478c027409710c7
dat09loz <isaacnguyen1308@gmail.com>
2022-09-03 22:44:34 +1000

    search page added frontend

---
+++ frontend/src/pages/Search.tsx
@@ -0,0 +1,11 @@
+// import styles from "../styles/Search.module.css";
+
+const Search = () => {
+  return(
+    <div className="container px-3 py-2 mx-auto lg:pt-8 lg:px-32">
+      <h1 className="font-bold text-2xl">Search results for: </h1>
+    </div>
+  )
+}
+
+export default Search;
