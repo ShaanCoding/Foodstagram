5a954fe173b7ac76bbcf09fdee4870a076336252
alwin5021 <alwinmoncy0@gmail.com>
2022-09-30 04:24:50 +1000

    Worked on create post feature

--- backend/routes/main.ts
+++ backend/routes/main.ts
@@ -1,4 +1,8 @@
-import { Router } from 'express'
+import { DefaultAzureCredential } from "@azure/identity";
+import { BlobServiceClient } from "@azure/storage-blob";
+import express, { Request, Response, Router } from 'express'
+
+import { blobServiceClient, database } from '../app'
 import { hello } from './hello'
 import { index } from './index'
 
@@ -7,4 +11,69 @@ const router = Router()
 router.get('/', index)
 router.get('/hello/:name', hello)
 
+router.post('/posts', (_req: Request, res: Response) => {
+	const req = _req as Request & { files: Record<string, unknown> };
+
+	const { picture, caption, location }: { picture: unknown, caption: string, location: string } = { ...req.body, ...req.files };
+
+	if(!picture || !caption || !location) return res.send('missing fields')
+
+	// see who is logged in (check cookies / token)
+
+	// 0. extract gps coordinates from the picture
+
+	// 1. create a post
+
+	database.query("INSERT INTO `posts` (`account_id`, `location_name`, `location_lat`, `location_long`, `caption`, `created_at`, `updated_at`) VALUES ( :account_id, :location_name, :location_lat, :location_long, :caption, NOW(), NOW() );", {
+		account_id: 1,
+		location_name: location,
+		location_lat: 0.1,
+		location_long: 0.1,
+		caption,
+	}, async (err, result) => {
+
+		if(err) {
+			console.log(err)
+			return res.send('error')
+		}
+
+
+		console.log("Available containers")
+		for await (const container of blobServiceClient.listContainers()) {
+			console.log(`Container: ${container.name}`);
+		}
+
+		/*
+		const containerClient = blobServiceClient.getContainerClient("<container name>");
+
+		const picture = Buffer.from([]);
+		const picture_name = "name of the picture";
+
+		const blockBlobClient = containerClient.getBlockBlobClient(picture_name);
+		const uploadBlobResponse = await blockBlobClient.upload(picture, picture.length);
+
+		console.log(`Upload block blob ${picture_name} successfully`, uploadBlobResponse.requestId);
+		*/
+
+		    /**
+			 * User can select a file ✅
+			 * User previews the file ❌
+			 * User sends the form ✅❌
+			 * The server recieves the request ✅
+			 * The server connects to the database ✅
+			 * The server upload the picture to the bucket ❌
+			 * The server stores the post_image_url in the database ❌
+			 * The server stores the post in the database ❌
+			 * The server responds that everything went well ❌
+			 */
+		
+		// 2 upload picture to the bucket and get the url back?
+	
+		// 3. create a post_image
+		res.send('ok')
+	});
+	
+})
+
 export default router
+
--- frontend/src/components/common/CreatePostModal.tsx
+++ frontend/src/components/common/CreatePostModal.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, MutableRefObject } from "react";
 import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { light, regular, solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { MutableRefObject, useEffect, useState } from "react";
+import { Modal } from 'react-responsive-modal';
+
 interface Props {
     openButton: MutableRefObject<any>
 }
@@ -21,7 +23,6 @@ const CreatePostModal = (props: Props) => {
 
     }, [openButton])
 
-
     return (
         <div>
             <Modal classNames = {{modal:"rounded-md w-4/5 h-4/5"}} open={open} showCloseIcon={false} onClose={onCloseModal} center>
@@ -30,22 +31,33 @@ const CreatePostModal = (props: Props) => {
 
                 <div className="flex flex-col justify center items-center h-full">
                     <h1 className="mb-3">Create a Post</h1>
-                    <div className="w-full bg-gray-200 h-[0.5px]" />
-                    <div className="w-full h-full flex justify-center items-center flex-col">
-                        <FontAwesomeIcon
-                            className="w-24 h-24"
-                            icon={
-                                regular("images")
-                            }
-                        />
-                        <p className="text-xl font-extralight mt-4 mb-4">Drag photos and videos here</p>
+                    <form action="http://localhost:3001/posts" method="POST">
+                        <div className="w-full bg-gray-200 h-[0.5px]" />
+                            <div className="w-full h-full flex justify-center items-center flex-col">
+                                <FontAwesomeIcon
+                                    className="w-24 h-24"
+                                    icon={
+                                        regular("images")
+                                    }
+                                />
+                                <p className="text-xl font-extralight mt-4 mb-4">Drag photos and videos here</p>
+                                
 
-                        <div>
-                        <button type="button" className="bg-insta-green px-3 py-1 text-white rounded mt-3">Select from computer</button>
+                                <div>
+                                    
+                                <input type="file" accept="image/png, image/jpeg, image/*" name="picture" />
+                                <label>Caption:</label>
+                                <input type="text" name="caption" />
+                                <label>Location:</label>
+                                <input type="text" name="location" />
 
-                    </div>
-    
-                    </div>
+                                <input type="submit" /> 
+
+                                <button type="button" className="bg-insta-green px-3 py-1 text-white rounded mt-3">Select from computer</button>
+
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </Modal>
         </div>
