6f30d7dc603d60cc410bcd09059078f48ef4baef
brandonjury <13889264@student.uts.edu.au>
2022-09-30 15:40:46 +1000

    Modified db and Feed

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -6,7 +6,8 @@ import post2 from "../images/post2.jpg";
 import avatar2 from "../images/avatar2.jpg";
 import post3 from "../images/post3.jpg";
 import avatar3 from "../images/avatar3.jpg";
-// import like from "../images/like.png" // use this for like button (or find a new icon, then find the same icon filled in, so when you click like it becomes solid)
+import like from "../images/like.png"; // use this for like button (or find a new icon, then find the same icon filled in, so when you click like it becomes solid)
+import save from "../images/save.png";
 // import posts from './db.json'
 
 const Home = () => {
@@ -29,19 +30,46 @@ const Home = () => {
 						src={post1}
 					/>
 
+          
           {/* Add like and maybe comment buttons here. Then add a save button on the right hand side*/}
+          <span className="flex items-stretch">
+            <img
+            alt="Like"
+            className="mb-4 h-5 inline-block pr-5"
+            src={like}
+          />
 
+          <img
+            alt="Save"
+            className="mb-4 h-5 inline-block"
+            src={save}
+          />
+          </span>
+          
         <p className="text-sm text-black-500 font-medium text-left inline-block align-middle mb-4 mr-2">
           skyemcalpine says:
         </p>        
         <p className="text-sm text-black-500 text-left inline-block align-middle mb-4">
         Making strawberry and vodka jam 🍓
 				</p>
+        <hr className="border-y-1 w-80 mb-4 mx-auto"></hr>
+        <p className="text-sm text-black-500 font-medium text-left inline-block align-middle mb-4 mr-2">
+          kanyewest says:
+        </p>        
+        <p className="text-sm text-black-500 text-left inline-block align-middle mb-4">
+        Wow!!!
+				</p>
+        <br></br>
+        <p className="text-sm text-black-500 font-medium text-left inline-block align-middle mb-4 mr-2">
+          zuck says:
+        </p>        
+        <p className="text-sm text-black-500 text-left inline-block align-middle mb-4">
+        Delicious.. I have to try this!
+				</p>
 
         <form className="flex flex-row">
-
-        <label className="flex-auto block p-3 bg-gray-100 rounded-2xl">
-          <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" value="Type a comment..." />
+        <label className="flex-auto block p-1 bg-gray-100 rounded-xl">
+          <input className="pl-2 w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" placeholder="Type a comment..." />
         </label>
             <button className="ml-1 bg-insta-green text-white text-sm p-[7px] rounded-md font-medium inline-block h-auto w-1/6" type="submit">
               Post
@@ -75,12 +103,41 @@ const Home = () => {
 
     {/* Add like and maybe comment buttons here. Then add a save button on the right hand side*/}
 
+    <span className="flex items-stretch">
+            <img
+            alt="Like"
+            className="mb-4 h-5 inline-block pr-5"
+            src={like}
+          />
+
+          <img
+            alt="Save"
+            className="mb-4 h-5 inline-block"
+            src={save}
+          />
+          </span>
+
   <p className="text-sm text-black-500 font-medium text-left inline-block align-middle mb-4 mr-2">
     therock says:
   </p>        
   <p className="text-sm text-black-500 text-left inline-block align-middle mb-4">
-  Immaculate!
+  Immaculate.
   </p>
+
+  <hr className="border-y-1 w-80 mb-4 mx-auto"></hr>
+        <p className="text-sm text-black-500 font-medium text-left inline-block align-middle mb-4 mr-2">
+          kanyewest says:
+        </p>        
+        <p className="text-sm text-black-500 text-left inline-block align-middle mb-4">
+        Wow!!!
+				</p>
+        <br></br>
+        <p className="text-sm text-black-500 font-medium text-left inline-block align-middle mb-4 mr-2">
+          zuck says:
+        </p>        
+        <p className="text-sm text-black-500 text-left inline-block align-middle mb-4">
+        Delicious.. I have to try this!
+				</p>
 
   <form className="flex flex-row">
 
