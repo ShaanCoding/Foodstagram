f5cda6a096c2a43c88976e46c4b5d02717efe145
Jack Donaldson <jackdonaldson005@gmail.com>
2022-09-30 15:20:53 +1000

    Added azure blob auth & connection

--- backend/src/routes/main.ts
+++ backend/src/routes/main.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator'
 import { Hello } from './hello'
 import { Index } from './index'
 import { Login } from './login'
+import { Post } from './post'
 import { Register } from './register'
 
 const router = Router()
@@ -17,6 +18,14 @@ router.post(
 	body('username').isLength({ min: 5, max: 80 }),
 	body('password').isStrongPassword(),
 	Register
+)
+
+router.get(
+	'/post',
+	body('picture').isLength({ min: 5 }),
+	body('caption').isLength({ min: 5 }),
+	body('location').isLength({ min: 5 }),
+	Post
 )
 
 router.post('/login', body('email').isEmail(), body('password'), Login)
