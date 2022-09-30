5a4fa7bcac7af5732270e773a05b93eaad806b99
brandonjury <13889264@student.uts.edu.au>
2022-09-30 21:51:08 +1000

    Added feed query

--- backend/src/routes/feed.ts
+++ backend/src/routes/feed.ts
@@ -3,9 +3,15 @@ import { json, Request, Response } from 'express'
 // import { validationResult } from 'express-validator'
 // import formatErrors from '../util/formatErrors'
 
+// WHERE account_id = (SELECT account_id FROM accounts WHERE )
+
+// Step 1: get the following account IDs (hardcoded for now)
+// SELECT followed_account_id FROM account_followers WHERE account_id = 1
+// Step 2: get the post IDs based on each following account ID
+// Step 3: get the post data based on each post ID
+// Step 4: order by post date
 const feedQuery = `
-	SELECT post_id, account_id, location_name, location_lat, location_long, caption, created_at, updated_at FROM posts AS posts_table,
-	SELECT profile_picture_url FROM accounts AS accounts_table
+	SELECT post_id, account_id, location_name, location_lat, location_long, caption, created_at, updated_at, (SELECT profile_picture_url FROM accounts WHERE account_id IN (1, 2, 3)) FROM posts WHERE account_id IN (1, 2, 3) AS posts_table
 `
 
 async function Feed(req: Request, res: Response) {
