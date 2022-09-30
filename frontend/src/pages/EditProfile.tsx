1903bfc278b5819d0d76eb277119bc04a4717726
jibreil <jibreilhoneine@gmail.com>
2022-09-30 18:34:08 +1000

    edit profile db base

--- frontend/src/pages/EditProfile.tsx
+++ frontend/src/pages/EditProfile.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "../styles/Profile.module.css";
 import { Link } from "react-router-dom";
+import Form from '../components/form/Form'
 import UseProfileQuery from "../api/UseProfileQuery";
+import UseEditProfileMutation from "../api/UseEditProfileMutation";
 
 const EditProfile = () => {
 
     const profileQuery = UseProfileQuery("1")
+    const editProfileMutation = UseEditProfileMutation()
 
     return (
         <div className="relative max-w-2xl mx-auto my-3">
@@ -18,62 +21,72 @@ const EditProfile = () => {
 
                         <form className="mx-5 my-5">
 
-                            <div className="grid grid-cols-5 py-3 mb-5">
-                                <div></div>
-                                <div className="block pt-2 place-self-end self-center font-semibold">
-                                    <p>Profile Picture</p>
+                            <Form
+                                onSubmit={(data) => {
+                                    editProfileMutation.mutate({
+                                        fullName: data['fullName'],
+                                        username: data['username'],
+                                        email: data['email'],
+                                        password: data['password'],
+                                    })
+                                }}
+                            >
+                                <div className="grid grid-cols-5 py-3 mb-5">
+                                    <div></div>
+                                    <div className="block pt-2 place-self-end self-center font-semibold">
+                                        <p>Profile Picture</p>
+                                    </div>
+                                    <div className="place-self-end px-4">
+                                        <img className="h-20 w-20 object-cover rounded-full place-items-center" src="https://source.unsplash.com/8hI_OW99d28" alt="Current profile photo" />
+                                    </div>
+                                    <label className="block pt-2 self-center col-span-2">
+                                        <span className="sr-only t-2">Choose profile photo</span>
+                                        <input type="file" className="w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-insta-green file:text-white hover:file:bg-green-500" />
+                                    </label>
                                 </div>
-                                <div className="place-self-end px-4">
-                                    <img className="h-20 w-20 object-cover rounded-full place-items-center" src="https://source.unsplash.com/8hI_OW99d28" alt="Current profile photo" />
-                                </div>
-                                <label className="block pt-2 self-center col-span-2">
-                                    <span className="sr-only t-2">Choose profile photo</span>
-                                    <input type="file" className="w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-insta-green file:text-white hover:file:bg-green-500" />
+
+                                <label className="relative block p-3 bg-gray-100 rounded-2xl">
+                                    <span className="text-md font-semibold text-zinc-900">
+                                        Username
+                                    </span>
+                                    <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" placeholder="Your username" value={profileQuery.data?.data.data.username} />
                                 </label>
-                            </div>
 
-                            <label className="relative block p-3 bg-gray-100 rounded-2xl">
-                                <span className="text-md font-semibold text-zinc-900">
-                                    Username
-                                </span>
-                                <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" placeholder="Your username" value={profileQuery.data?.data.data.username} />
-                            </label>
+                                <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
+                                    <span className="text-md font-semibold text-zinc-900">
+                                        Name
+                                    </span>
+                                    <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" placeholder="Your name" value={profileQuery.data?.data.data.name} />
+                                </label>
 
-                            <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
-                                <span className="text-md font-semibold text-zinc-900">
-                                    Name
-                                </span>
-                                <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" placeholder="Your name" value={profileQuery.data?.data.data.name} />
-                            </label>
+                                <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
+                                    <span className="text-md font-semibold text-zinc-900">
+                                        Profile description
+                                    </span>
+                                    <textarea id="profileDescription" rows={5} className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" placeholder="Profile description">{profileQuery.data?.data.data.bio}</textarea>
+                                </label>
 
-                            <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
-                                <span className="text-md font-semibold text-zinc-900">
-                                    Profile description
-                                </span>
-                                <textarea id="profileDescription" rows={5} className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" placeholder="Profile description">{profileQuery.data?.data.data.bio}</textarea>
-                            </label>
+                                <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
+                                    <span className="text-md font-semibold text-zinc-900">
+                                        Email
+                                    </span>
+                                    <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" placeholder="Your email address" value={profileQuery.data?.data.data.email} />
+                                </label>
 
-                            <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
-                                <span className="text-md font-semibold text-zinc-900">
-                                    Email
-                                </span>
-                                <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" placeholder="Your email address" value={profileQuery.data?.data.data.email} />
-                            </label>
+                                <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
+                                    <span className="text-md font-semibold text-zinc-900">
+                                        Password
+                                    </span>
+                                    <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="password" placeholder="Your password" />
+                                </label>
 
-                            <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
-                                <span className="text-md font-semibold text-zinc-900">
-                                    Password
-                                </span>
-                                <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="password" placeholder="Your password" />
-                            </label>
-
-                            <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
-                                <span className="text-md font-semibold text-zinc-900">
-                                    Phone Number
-                                </span>
-                                <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" placeholder="Your phone number" value="+61 423 456 789" />
-                            </label>
-
+                                <label className="relative block p-3 bg-gray-100 rounded-2xl mt-5">
+                                    <span className="text-md font-semibold text-zinc-900">
+                                        Phone Number
+                                    </span>
+                                    <input className="w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none" id="name" type="text" placeholder="Your phone number" value="+61 423 456 789" />
+                                </label>
+                            </Form>
                         </form>
 
                         <div className="flex space-x-4">
