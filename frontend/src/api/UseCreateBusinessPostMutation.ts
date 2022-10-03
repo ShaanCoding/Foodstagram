83a5ac724e872f9d231e769241f8f033cd5c7268
Shaan <shaankhan101@gmail.com>
2022-10-03 23:26:58 +1100

    Added changes -> Made it ability to post

---
+++ frontend/src/api/UseCreateBusinessPostMutation.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { useMutation, useQuery } from "react-query";
+
+import GetEndpoint from "./util/GetEndpoint";
+
+export interface CreateNewBusinessPost {
+  picture: string;
+  caption: string;
+  location: string;
+  businessState: number;
+  dateTime: string;
+}
+
+export function UseCreateBusinessPostMutation() {
+  return useMutation(
+    ["createpost"],
+    (variables: CreateNewBusinessPost) =>
+      axios.post(`${GetEndpoint("api")}/businessPosts`, variables),
+    { retry: false }
+  );
+}
