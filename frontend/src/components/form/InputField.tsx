9a42f4b9ea5f198a31fc80d426dd6cab23e158cc
Jack Donaldson <jackdonaldson005@gmail.com>
2022-10-03 20:10:16 +1100

    Change to use post component

--- frontend/src/components/form/InputField.tsx
+++ frontend/src/components/form/InputField.tsx
@@ -2,18 +2,19 @@ import { useState } from 'react'
 
 import styles from '../../styles/Form.module.css'
 
-interface Props extends React.InputHTMLAttributes<HTMLInputElement>{
-	placeholder: string;
-	initialValue?: string;
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+	placeholder: string
+	initialValue?: string
 }
 
 const InputField = (props: Props) => {
 	const [value, setValue] = useState(props.initialValue)
 	return (
 		<input
-			{ ...props }
+			{...props}
 			className={`p-2 w-full rounded-sm mb-2 border hover:border-gray-500 focus-visible:border text-xs bg-white ${styles.greyBackground}`}
-			onChange={(event)=>{
+			value={value}
+			onChange={(event) => {
 				setValue(event.target.value)
 			}}
 		/>
