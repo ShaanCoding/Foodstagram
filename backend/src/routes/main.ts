22ac4344fd4060b0f8ad708f0a753b7812cc441d
Shaan <shaankhan101@gmail.com>
2022-10-03 23:25:39 +1100

    Added changes -> Made it ability to post

--- backend/src/routes/main.ts
+++ backend/src/routes/main.ts
@@ -1,104 +1,115 @@
-import Router from 'express-promise-router'
-import { body, param } from 'express-validator'
+import Router from "express-promise-router";
+import { body, param } from "express-validator";
 
-import { AuthenticateUser } from '../util/auth'
-import { EditProfile } from './editprofile'
-import { FollowerCount } from './followerCount'
-import { FollowingCount } from './followingCount'
-import { Hello } from './hello'
-import { Index } from './index'
-import { Login } from './login'
-import { Me } from './me'
-import { SearchUsers, SearchPosts } from './search'
-import { PostCount } from './postCount'
-import { ProfilePosts } from './profilePosts'
-import { GetPosts } from './feed'
-import { Follow } from './follow'
-import { Block } from './block'
-import { ProfilePic } from './profilePic'
-import { IsFollowing } from './isfollowing'
-import { IsBlocking } from './isblocking'
-import { CreatePost, DeletePost, UpdatePost } from './post'
-import { Profile } from './profile'
-import { Register } from './register'
+import { AuthenticateUser } from "../util/auth";
+import { EditProfile } from "./editprofile";
+import { FollowerCount } from "./followerCount";
+import { FollowingCount } from "./followingCount";
+import { Hello } from "./hello";
+import { Index } from "./index";
+import { Login } from "./login";
+import { Me } from "./me";
+import { SearchUsers, SearchPosts } from "./search";
+import { PostCount } from "./postCount";
+import { ProfilePosts } from "./profilePosts";
+import { GetPosts } from "./feed";
+import { Follow } from "./follow";
+import { Block } from "./block";
+import { ProfilePic } from "./profilePic";
+import { IsFollowing } from "./isfollowing";
+import { IsBlocking } from "./isblocking";
+import { CreatePost, DeletePost, UpdatePost } from "./post";
+import { Profile } from "./profile";
+import { Register } from "./register";
 
-const router = Router()
+const router = Router();
 
-router.get('/', Index)
+router.get("/", Index);
 //router.get('/*', GetAllPosts)
 
-router.get('/hello', Hello)
-router.get('/hello/:name', Hello)
+router.get("/hello", Hello);
+router.get("/hello/:name", Hello);
 
 router.post(
-	'/register',
-	body('email').isEmail(),
-	body('fullName').isLength({ min: 5, max: 120 }),
-	body('username').isLength({ min: 5, max: 80 }),
-	body('password').isStrongPassword(),
-	Register
-)
+  "/register",
+  body("email").isEmail(),
+  body("fullName").isLength({ min: 5, max: 120 }),
+  body("username").isLength({ min: 5, max: 80 }),
+  body("password").isStrongPassword(),
+  Register
+);
 
-router.get('/feed', AuthenticateUser, GetPosts)
+router.get("/feed", AuthenticateUser, GetPosts);
 
 router.post(
-	'/posts',
-	body('picture').isLength({ min: 5 }),
-	body('caption').isLength({ min: 5 }),
-	body('location').isLength({ min: 5 }),
-	CreatePost
-)
+  "/posts",
+  body("picture").isLength({ min: 5 }),
+  body("caption").isLength({ min: 5 }),
+  body("location").isLength({ min: 5 }),
+  CreatePost
+);
 router.put(
-	'/posts/:post_id',
-	param('post_id').isNumeric(),
-	body('caption').isLength({ min: 5 }),
-	body('location').isLength({ min: 5 }),
-	AuthenticateUser,
-	UpdatePost
-)
+  "/posts/:post_id",
+  param("post_id").isNumeric(),
+  body("caption").isLength({ min: 5 }),
+  body("location").isLength({ min: 5 }),
+  AuthenticateUser,
+  UpdatePost
+);
 router.delete(
-	'/posts/:post_id',
-	param('post_id').isNumeric(),
-	AuthenticateUser,
-	DeletePost
-)
+  "/posts/:post_id",
+  param("post_id").isNumeric(),
+  AuthenticateUser,
+  DeletePost
+);
 
-router.get('/me', AuthenticateUser, Me)
+router.get("/me", AuthenticateUser, Me);
 
-router.post('/login', body('email').isEmail(), body('password'), Login)
+router.post("/login", body("email").isEmail(), body("password"), Login);
 
-router.get('/profile/:username', Profile)
-router.get('/postCount/:profileID', PostCount)
-router.get('/followerCount/:profileID', FollowerCount)
-router.get('/followingCount/:profileID', FollowingCount)
-router.get('/profilePosts/:profileID', AuthenticateUser, ProfilePosts)
-
-router.post(
-	'/editprofile/:username',
-	body('email').isEmail(),
-	body('fullName').isLength({ min: 5, max: 120 }),
-	body('bio').isLength({ min: 5, max: 200 }),
-	body('username').isLength({ min: 5, max: 80 }),
-	body('phone').isLength({ min: 2, max: 15 }),
-	AuthenticateUser,
-	EditProfile
-)
+router.get("/profile/:username", Profile);
+router.get("/postCount/:profileID", PostCount);
+router.get("/followerCount/:profileID", FollowerCount);
+router.get("/followingCount/:profileID", FollowingCount);
+router.get("/profilePosts/:profileID", AuthenticateUser, ProfilePosts);
 
 router.post(
-	'/profilePic/:username',
-	body('picture').isLength({ min: 5 }),
-	ProfilePic
-)
+  "/editprofile/:username",
+  body("email").isEmail(),
+  body("fullName").isLength({ min: 5, max: 120 }),
+  body("bio").isLength({ min: 5, max: 200 }),
+  body("username").isLength({ min: 5, max: 80 }),
+  body("phone").isLength({ min: 2, max: 15 }),
+  AuthenticateUser,
+  EditProfile
+);
+
+router.post(
+  "/profilePic/:username",
+  body("picture").isLength({ min: 5 }),
+  ProfilePic
+);
 
 //search routes
-router.post('/api/search_user', body('searchStr'), SearchUsers)
-router.post('/api/search_post', body('searchStr'), SearchPosts)
+router.post("/api/search_user", body("searchStr"), SearchUsers);
+router.post("/api/search_post", body("searchStr"), SearchPosts);
 //end search routes
 
-router.get('/feed', AuthenticateUser, GetPosts)
-router.post('/follow', body('account_to_follow'), AuthenticateUser, Follow)
-router.post('/block', body('account_to_block'), AuthenticateUser, Block)
-router.get('/isfollowing/:account_id', AuthenticateUser, IsFollowing)
-router.get('/isblocking/:account_id', AuthenticateUser, IsBlocking)
+router.get("/feed", AuthenticateUser, GetPosts);
+router.post("/follow", body("account_to_follow"), AuthenticateUser, Follow);
+router.post("/block", body("account_to_block"), AuthenticateUser, Block);
+router.get("/isfollowing/:account_id", AuthenticateUser, IsFollowing);
+router.get("/isblocking/:account_id", AuthenticateUser, IsBlocking);
 
-export default router
+// Business posts
+router.post(
+  "/businessPosts",
+  body("picture").isLength({ min: 5 }),
+  body("caption").isLength({ min: 5 }),
+  body("location").isLength({ min: 5 }),
+  body("dateTime").isLength({ min: 5 }),
+  CreatePost
+);
+// End of business posts
+
+export default router;
--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -1,82 +1,84 @@
-import useAuth from '../api/util/useAuth'
-import UseFeedQuery from '../api/UseFeedQuery'
-import { Post } from '../components/post/Post'
+import useAuth from "../api/util/useAuth";
+import UseFeedQuery from "../api/UseFeedQuery";
+import styles from "../styles/Feed.module.css";
+import { Post } from "../components/post/Post";
+import like from "../images/icons/like.png";
+import save from "../images/icons/save.png";
+import trash from "../images/icons/trash.png";
 
 const Feed = () => {
-	const [account, isLoading] = useAuth()
-	const feedQuery = UseFeedQuery()
-	return (
-		<div>
-			{feedQuery.isLoading === false &&
-				feedQuery.isSuccess &&
-				feedQuery.data.data.posts === undefined && (
-					<h1 className="mx-auto block w-2/3 text-center mt-[200px] mb-[200px]">
-						Your feed appears to be empty, we recommend you follow some more
-						people!
-					</h1>
-				)}
-			{feedQuery.isLoading === false &&
-				feedQuery.isSuccess &&
-				feedQuery.data.data.posts !== undefined &&
-				feedQuery.data?.data.posts.map((post: Post) => (
-					<div className="m-12 w-4/5 h-full ml-auto mr-auto flex max-w-[550px]">
-						<div className={`flex-auto w-14`}>
-							<div className={`py-7 px-8 bg-white border ${styles.greyBorder}`}>
-								<img
-									alt="avatar"
-									className="w-8 h-8 rounded-full border-2 border-gray-700 inline-block align-middle mb-4"
-									src={post.profile_picture_url}
-								/>
-								<a
-									href={"/profile/" + post.username}
-									className="font-medium text-md text-black-500 text-left inline-block ml-4 align-middle mb-4"
-								>
-									{post.username}
-								</a>
-								<img alt="Post" className="mb-4" src={post.post_image} />
-								<span className="flex items-stretch">
-									<img
-										alt="Like"
-										className="mb-4 h-5 inline-block pr-5"
-										src={like}
-									/>
+  const [account, isLoading] = useAuth();
+  const feedQuery = UseFeedQuery();
+  return (
+    <div>
+      {feedQuery.isLoading === false &&
+        feedQuery.isSuccess &&
+        feedQuery.data.data.posts === undefined && (
+          <h1 className="mx-auto block w-2/3 text-center mt-[200px] mb-[200px]">
+            Your feed appears to be empty, we recommend you follow some more
+            people!
+          </h1>
+        )}
+      {feedQuery.isLoading === false &&
+        feedQuery.isSuccess &&
+        feedQuery.data.data.posts !== undefined &&
+        feedQuery.data?.data.posts.map((post: Post) => (
+          <div className="m-12 w-4/5 h-full ml-auto mr-auto flex max-w-[550px]">
+            <div className={`flex-auto w-14`}>
+              <div className={`py-7 px-8 bg-white border ${styles.greyBorder}`}>
+                <img
+                  alt="avatar"
+                  className="w-8 h-8 rounded-full border-2 border-gray-700 inline-block align-middle mb-4"
+                  src={post.profile_picture_url}
+                />
+                <a
+                  href={"/profile/" + post.username}
+                  className="font-medium text-md text-black-500 text-left inline-block ml-4 align-middle mb-4"
+                >
+                  {post.username}
+                </a>
+                <img alt="Post" className="mb-4" src={post.post_image} />
+                <span className="flex items-stretch">
+                  <img
+                    alt="Like"
+                    className="mb-4 h-5 inline-block pr-5"
+                  />
 
-									<img
-										alt="Save"
-										className="mb-4 h-5 inline-block"
-										src={save}
-									/>
-								</span>
+                  <img
+                    alt="Save"
+                    className="mb-4 h-5 inline-block"
+                  />
+                </span>
 
-								<p className="text-sm text-black-500 font-medium text-left inline-block align-middle mb-4 mr-2">
-									{post.username} says:
-								</p>
-								<p className="text-sm text-black-500 text-left inline-block align-middle mb-4">
-									{post.caption}
-								</p>
+                <p className="text-sm text-black-500 font-medium text-left inline-block align-middle mb-4 mr-2">
+                  {post.username} says:
+                </p>
+                <p className="text-sm text-black-500 text-left inline-block align-middle mb-4">
+                  {post.caption}
+                </p>
 
-								<form className="flex flex-row">
-									<label className="flex-auto block p-1 bg-gray-100 rounded-xl">
-										<input
-											className="pl-2 w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none"
-											id="name"
-											type="text"
-											placeholder="Type a comment..."
-										/>
-									</label>
-									<button
-										className="ml-1 bg-insta-green text-white text-sm p-[7px] rounded-md font-medium inline-block h-auto w-1/6"
-										type="submit"
-									>
-										Post
-									</button>
-								</form>
-							</div>
-						</div>
-					</div>
-				))}
-		</div>
-	)
-}
+                <form className="flex flex-row">
+                  <label className="flex-auto block p-1 bg-gray-100 rounded-xl">
+                    <input
+                      className="pl-2 w-full bg-transparent p-0 text-sm  text-gray-500 focus:outline-none"
+                      id="name"
+                      type="text"
+                      placeholder="Type a comment..."
+                    />
+                  </label>
+                  <button
+                    className="ml-1 bg-insta-green text-white text-sm p-[7px] rounded-md font-medium inline-block h-auto w-1/6"
+                    type="submit"
+                  >
+                    Post
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
