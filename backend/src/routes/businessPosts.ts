91fd8771e65cdfac8559248453bbc55b5efad800
Shaan <shaankhan101@gmail.com>
2022-10-23 19:42:17 +1100

    Added comments

--- backend/src/routes/businessPosts.ts
+++ backend/src/routes/businessPosts.ts
@@ -8,6 +8,7 @@ import formatErrors from "../util/formatErrors";
 import { getBlobClient } from "../util/storage";
 
 export async function CreateBusinessPost(req: Request, res: Response) {
+  // Validates body of request to make sure it is following validation rules
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(formatErrors(errors));
@@ -15,10 +16,10 @@ export async function CreateBusinessPost(req: Request, res: Response) {
 
   const postQuery =
     "INSERT INTO `posts` (`account_id`, `location_name`, `location_lat`, `location_long`, `caption`, `businessState`, `businessScheduleTime`, `created_at`, `updated_at`, `categories`) VALUES ( ?, ?, ?, ?, ?, ?, ?, NOW(), NOW(), ?)";
-
   const imageQuery = `INSERT INTO post_images (post_id, image_url) VALUES (?, ?)`;
 
   const { ENVIRONMENT } = process.env;
+  // Get the data from the request body
   const {
     picture,
     caption,
@@ -29,20 +30,21 @@ export async function CreateBusinessPost(req: Request, res: Response) {
     categories,
   } = req.body;
 
+  // uuidv4() generates a random string for the blob name
   const pictureName = uuidv4();
   const pictureBuffer = Buffer.from(picture, "base64");
 
   const lognitude = 0.1;
   const latitude = 0.1;
 
-  validationResult(req).throw();
-
   try {
+    // Upload the picture to the blob client
     getBlobClient()
       .getContainerClient(ENVIRONMENT!)
       .getBlockBlobClient(pictureName)
       .upload(pictureBuffer, pictureBuffer.length);
 
+    // Create the post in the database
     let post = (await Query(postQuery, [
       account_id ? account_id : 1,
       location,
@@ -54,20 +56,22 @@ export async function CreateBusinessPost(req: Request, res: Response) {
       categories,
     ])) as any;
 
-    console.log(post.insertId);
-
+    // Updates the post with the picture url
     await Query(imageQuery, [
       post.insertId,
       `https://asdbackend.blob.core.windows.net/${ENVIRONMENT}/${pictureName}`,
     ]);
 
+    // Return the post
     return res.status(201).json({ message: "Succesfully created post!" });
   } catch (ex) {
+    // If there is an error, return the error
     return res.status(500).json({ message: "Failed to create post" });
   }
 }
 
 export async function UpdateBusinessPost(req: Request, res: Response) {
+  // Validates request checking that the request body follows correct validation rules
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(formatErrors(errors));
@@ -79,6 +83,7 @@ export async function UpdateBusinessPost(req: Request, res: Response) {
     "UPDATE `posts` SET `caption` = ?, `location_name` = ?, `updated_at` = NOW(), `businessState` = ?, `categories` = ? WHERE `post_id` = ?";
 
   const post_id = req.params.post_id;
+
   const {
     caption,
     location,
@@ -88,6 +93,7 @@ export async function UpdateBusinessPost(req: Request, res: Response) {
   } = req.body;
 
   try {
+    // If it's a scheduled post we need to update the date time
     if (dateTime) {
       await Query(updateBusinessPostQuery, [
         caption,
@@ -97,19 +103,21 @@ export async function UpdateBusinessPost(req: Request, res: Response) {
         categories,
         post_id,
       ]);
+      // If it's not a scheduled post we don't need to update the date time
     } else {
-      let update = await Query(updateBusinessPostQueryNoDateTime, [
+      await Query(updateBusinessPostQueryNoDateTime, [
         caption,
         location,
         businessState,
         categories,
         post_id,
       ]);
-      console.log(update);
     }
 
+    // Return the updated post
     return res.status(201).json({ message: "Successfully updated post! " });
   } catch (ex) {
+    // Error if query fails
     console.log(ex);
     res.status(500).json({ message: "Failed to update post" });
   }
