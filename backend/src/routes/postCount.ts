a899933635ae66de385222316cf7da0abd56d9e1
jiak1 <jackdonaldson005@gmail.com>
2022-10-25 13:58:31 +1100

    Fix posts not showing on correct account

--- backend/src/routes/postCount.ts
+++ backend/src/routes/postCount.ts
@@ -1,34 +1,34 @@
-import { json, Request, Response } from "express";
-import { Query } from "../util/db";
-import { validationResult } from "express-validator";
-import formatErrors from "../util/formatErrors";
+import { json, Request, Response } from 'express'
+import { Query } from '../util/db'
+import { validationResult } from 'express-validator'
+import formatErrors from '../util/formatErrors'
 
 const PostCountQuery = `
-SELECT COUNT(*) as count FROM posts WHERE account_id = ? AND businessState IS NULL OR businessState = 1;
-`;
+SELECT COUNT(*) as count FROM posts WHERE account_id = ? AND (businessState IS NULL OR businessState = 1);
+`
 
 async function PostCount(req: Request, res: Response) {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(formatErrors(errors));
-  }
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		return res.status(400).json(formatErrors(errors))
+	}
 
-  try {
-    const rows = (await Query(PostCountQuery, [
-      req.params.profileID,
-    ])) as Account[];
+	try {
+		const rows = (await Query(PostCountQuery, [
+			req.params.profileID,
+		])) as Account[]
 
-    if (rows.length > 0) {
-      return res.status(200).json({
-        message: "Post count",
-        data: rows[0],
-      });
-    }
-  } catch {
-    return res
-      .status(500)
-      .json({ message: "Failed to connect to the database" });
-  }
+		if (rows.length > 0) {
+			return res.status(200).json({
+				message: 'Post count',
+				data: rows[0],
+			})
+		}
+	} catch {
+		return res
+			.status(500)
+			.json({ message: 'Failed to connect to the database' })
+	}
 }
 
-export { PostCount };
+export { PostCount }
